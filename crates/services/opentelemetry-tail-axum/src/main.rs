cf1b6c5c2657ccfdcca201a50b18c43808e85fbf
apo9666 <66564120+apo9666@users.noreply.github.com>
2024-07-13 15:19:50 -0300

    Add otel http logs

--- crates/services/opentelemetry-tail-axum/src/main.rs
+++ crates/services/opentelemetry-tail-axum/src/main.rs
@@ -3,13 +3,18 @@ use axum::{
         ws::{Message, WebSocket},
         ConnectInfo, State, WebSocketUpgrade,
     },
+    http::StatusCode,
     response::IntoResponse,
-    routing::get,
+    routing::{get, post},
     Router,
 };
+use axum_extra::protobuf::Protobuf;
 use futures::{sink::SinkExt, stream::StreamExt};
+use opentelemetry_proto::tonic::logs::v1::LogsData;
 use std::{net::SocketAddr, sync::Arc};
 use tokio::sync::broadcast;
+use tower_http::trace::{self, TraceLayer};
+use tracing::{info, Level};
 
 struct AppState {
     tx: broadcast::Sender<String>,
@@ -17,12 +22,23 @@ struct AppState {
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt().with_target(false).pretty().init();
+
     let (tx, _rx) = broadcast::channel(100);
     let app_state = Arc::new(AppState { tx });
 
     let app = Router::new()
         .route("/ws", get(ws_handler))
-        .route("/v1/logs", get(otel_handler))
+        .route("/v1/logs", post(otel_handler))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(
+                    trace::DefaultMakeSpan::new()
+                        .level(Level::INFO)
+                        .include_headers(true),
+                )
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        )
         .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -37,10 +53,14 @@ async fn main() {
     .unwrap();
 }
 
-async fn otel_handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    let _ = state.tx.send("banana".into());
+async fn otel_handler(
+    State(state): State<Arc<AppState>>,
+    Protobuf(payload): Protobuf<LogsData>,
+) -> impl IntoResponse {
+    info!("{:?}", serde_json::to_string(&payload).unwrap());
+    let _ = state.tx.send(serde_json::to_string(&payload).unwrap());
 
-    "banana"
+    (StatusCode::OK, "Ok")
 }
 
 async fn ws_handler(
